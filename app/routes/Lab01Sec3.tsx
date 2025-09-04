16cb23847d8a7583e2985402a0a20e8c7a931448
ratthanun1008-1 <ratthanun.ruk@rmutto.ac.th>
2025-09-04 17:36:32 +0700

    Lab01Sec3

--- app/routes/Lab01Sec3.tsx
+++ app/routes/Lab01Sec3.tsx
@@ -2,39 +2,82 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 export default function MyExamProject(){
 
-  const myPage ="Ratthanun Term Project";
+  const myPage ="TCG SHOP";
     const myName ="Ratthanun Rukkarn";
     const myStudID ="026730491008-1";
     const myExamProject =[
     {
-    tpId: 1,
-    tpTitle: "Colleen",
-    tpDescription: "Colleen Shop",
-    tpLogo: "/images/colorPencil.png",
-    tpWebsite: "https://www.colleenpencil.co.th/",
-    tpTop10: true
+    Code: 1,
+    Title: "Cardfight Vanguard",
+    Description: "CD Booster Set 05-2",
+    Logo: "/images/D-BT05-2.png",
+    Category: 1,
+    Price: 150,
+    Owner: "Bushiroad",
+    Website: "https://www.thaivanguard.net/",
+    Top10: true
     },
     {
-    tpId: 2,
-    tpTitle: "Pen and Pencil Shop",
-    tpDescription: "Studying Shop",
-    tpLogo: "/images/pen.png",
-    tpWebsite: "https://www.b2s.co.th/en/" ,
-    tpTop10: false
+    Code: 2,
+    Title: "YUGIOH",
+    Description: "Duelist's Advance",
+    Logo: "/images/DUAD_550.png",
+    Category: 1,
+    Price: 70,
+    Owner: "Bushiroad",
+    Website: "https://www.yugioh-card.com/en/",
+    Top10: false
+    },
+    {
+    Code: 3,
+    Title: "BuddyFight",
+    Description: "BFN-BT03",
+    Logo: "/images/BFN-BT03.png",
+    Category: 1,
+    Price: 190,
+    Owner: "Bushiroad",
+    Website: "https://thaibuddyfight.com/",
+    Top10: true
+    },
+    {
+    tCode: 4,
+    Title: "Battle Spirits",
+    Description: "DBD-2-1",
+    Logo: "/images/DBD-2-1.png",
+    Category: 1,
+    Price: 200,
+    Owner: "Bandai",
+    Website: "https://www.battlespirits.com/",
+    Top10: false
+    },
+    {
+    Code: 5,
+    Title: "UNO",
+    Description: "Uno No Mercy",
+    Logo: "/images/UNO_No_Mercy.png",
+    Category: 1,
+    Price: 300,
+    Owner: "Mattel",
+    Website: "https://shop.mattel.com/collections/uno",
+    Top10: true
     }
 ];
 
     const examProj = myExamProject.map((tpObj, index) =>
     <div className="grid grid-cols-5 mx-auto border-gray-300">
     <div className="w-32 p-1">
-    <img src={tpObj.tpLogo} className="w-full rounded-full" title={tpObj.tpDescription+" ID: " +tpObj.tpId} />
+    <img src={tpObj.Logo} className="w-full rounded-full" title={tpObj.Description+" ID: " +tpObj.Code} />
     </div>
     <div className="p-4 col-span-3 mb-auto">
-    <h2 className="text-lg font-semibold text-gray-800">{tpObj.tpDescription}</h2>
-    <p className="text-gray-600 text-sm mt-2">{tpObj.tpTitle}</p>
+    <h2 className="text-lg font-semibold text-gray-800">{tpObj.Description}</h2>
+    <p className="text-gray-600 text-sm mt-2">{tpObj.Title}</p>
+    <p className="text-gray-600 text-sm mt-2">{tpObj.Category}</p>
+    <p className="text-gray-600 text-sm mt-2">{tpObj.Price}</p>
+    <p className="text-gray-600 text-sm mt-2">{tpObj.Owner}</p>
+
     </div>
     <div className="p-4 mb-auto">
-    <a href={tpObj.tpWebsite} className="bg-indigo-600 text-white rounded-sm hover:bg-indigo-700 px-10 py-2">Preview</a>
+    <a href={tpObj.Website} className="bg-indigo-600 text-white rounded-sm hover:bg-indigo-700 px-10 py-2">Preview</a>
     </div>
     </div>
     );
---
+++ app/routes/components/data/examProject.tsx
@@ -0,0 +1,57 @@
+export const myExamProject =[
+    {
+    Code: 1,
+    Title: "Cardfight Vanguard",
+    Description: "CD Booster Set 05-2",
+    Logo: "/images/D-BT05-2.png",
+    Category: 1,
+    Price: 150,
+    Owner: "Bushiroad",
+    Website: "https://www.thaivanguard.net/",
+    Top10: true
+    },
+    {
+    Code: 2,
+    Title: "YUGIOH",
+    Description: "Duelist's Advance",
+    Logo: "/images/DUAD_550.png",
+    Category: 1,
+    Price: 70,
+    Owner: "Bushiroad",
+    Website: "https://www.yugioh-card.com/en/",
+    Top10: false
+    },
+    {
+    Code: 3,
+    Title: "BuddyFight",
+    Description: "CD Booster Set 05-2",
+    Logo: "/images/D-BT05-2.png",
+    Category: 1,
+    Price: 190,
+    Owner: "Bushiroad",
+    Website: "https://thaibuddyfight.com/",
+    Top10: true
+    },
+    {
+    tCode: 4,
+    Title: "Battle Spirits",
+    Description: "DBD-2-1",
+    Logo: "/images/DBD-2-1.png",
+    Category: 1,
+    Price: 200,
+    Owner: "Bandai",
+    Website: "https://www.battlespirits.com/",
+    Top10: false
+    },
+    {
+    Code: 5,
+    Title: "UNO",
+    Description: "Uno No Mercy",
+    Logo: "/images/UNO_No_Mercy.png",
+    Category: 1,
+    Price: 300,
+    Owner: "Mattel",
+    Website: "https://shop.mattel.com/collections/uno",
+    Top10: true
+    }
+];
